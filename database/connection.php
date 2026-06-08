776bd608f972efc20341ec9721ef3357823c9886
Gobinda Ghimire <gowingg1@gmail.com>
2026-06-08 22:40:39 +0545

    edit on the connection code..

--- database/connection.php
+++ database/connection.php
@@ -1,9 +1,13 @@
 <?php
-
-$conn = mysqli_connect("localhost", "root", "", "intern_lab");
+$hname = "localhost";
+$uname = "root";
+$pass = "";
+$db = "intern_lab"
+    
+$conn = mysqli_connect($hname, $uname, $pass, $db);
 
 if (!$conn) {
-    die("Database Connection Failed");
+    die("Database Connection Failed: ".mysqli_connect_error());
 }
 
 ?>
